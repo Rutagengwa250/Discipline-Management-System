37fe805a8939711d65a0d3eb3a388b8a2a66484c
ruti <rutagengwa36@gmail.com>
2025-09-28 05:31:11 -0700

    changing environment

--- admin/emailService.js
+++ admin/emailService.js
@@ -7,12 +7,13 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendOTPEmail = async (email, otp) => {
   try {
-    await resend.emails.send({
-      from: 'no-reply@discipline-management-system-production.up.railway.app',
-      to: email,
-      subject: 'Your OTP for School Management System',
-      html: `<p>Your OTP is: <strong>${otp}</strong></p><p>This OTP is valid for 5 minutes.</p>`,
-    });
+   await resend.emails.send({
+  from: 'onboarding@resend.dev',  // test sender
+  to: 'delivered@resend.dev',     // test recipient provided by Resend
+  subject: 'Your OTP for School Management System',
+  html: `<p>Your OTP is: <strong>${otp}</strong>   it is valid for 5 minutes</p>`,
+});
+
     console.log('OTP email sent successfully');
   } catch (error) {
     console.error('Error sending OTP email:', error);
